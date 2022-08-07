7b5199f173d34002e888712315c950c53be3f309
rastopyr <rastopyr@gmail.com>
2022-08-07 22:13:04 +0200

    patch: menu markup

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,28 +1,73 @@
 import React from "react";
-import { Container, MenuItem, MenuList, Paper } from "@mui/material";
+import { Container, Grid, MenuItem, MenuList, Paper } from "@mui/material";
 import { NavigateTo } from "../../routing/NavigatoTo/NavigateTo";
 import { Route } from "../../routing/types";
 
+type MenuNavigationItem = {
+    readonly route: Route;
+    readonly title: string;
+};
+
+type MenuProps = {
+    readonly leftMenu: readonly MenuNavigationItem[];
+    readonly rightMenu: readonly MenuNavigationItem[];
+};
+
+export const MenuGrid: React.FC<MenuProps> = ({ leftMenu, rightMenu }) => (
+    <MenuList dense={true}>
+        <Grid container justifyContent="space-between" columns={leftMenu.length + rightMenu.length}>
+            <Grid item xs={leftMenu.length - 1}>
+                <Grid container columns={leftMenu.length - 1}>
+                    {leftMenu.map(item => (
+                        <Grid item xs={0.5}>
+                            <NavigateTo route={item.route}>
+                                <MenuItem>{item.title}</MenuItem>
+                            </NavigateTo>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Grid>
+
+            <Grid item xs={rightMenu.length - 1}>
+                <Grid container columns={rightMenu.length - 1}>
+                    {rightMenu.map(item => (
+                        <Grid item xs={0.5}>
+                            <NavigateTo route={item.route}>
+                                <MenuItem>{item.title}</MenuItem>
+                            </NavigateTo>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Grid>
+        </Grid>
+    </MenuList>
+);
+
+const LeftMenu: readonly MenuNavigationItem[] = [
+    {
+        route: Route.Home,
+        title: "Home"
+    },
+    {
+        route: Route.Profile,
+        title: "Profile"
+    }
+];
+
+export const RightMenu: readonly MenuNavigationItem[] = [
+    {
+        route: Route.SignIn,
+        title: "SignIn"
+    },
+    {
+        route: Route.SignUp,
+        title: "SignUp"
+    }
+];
 export const Header: React.FC = () => (
     <Paper elevation={1}>
         <Container>
-            <MenuList dense={true}>
-                <NavigateTo route={Route.Home}>
-                    <MenuItem>Home</MenuItem>
-                </NavigateTo>
-
-                <NavigateTo route={Route.Profile}>
-                    <MenuItem>Profile</MenuItem>
-                </NavigateTo>
-
-                <NavigateTo route={Route.SignUp}>
-                    <MenuItem>SignUp</MenuItem>
-                </NavigateTo>
-
-                <NavigateTo route={Route.SignIn}>
-                    <MenuItem>SignIn</MenuItem>
-                </NavigateTo>
-            </MenuList>
+            <MenuGrid leftMenu={LeftMenu} rightMenu={RightMenu} />
         </Container>
     </Paper>
 );
---
+++ src/plan.md
@@ -0,0 +1,23 @@
+
+1. Navigation
+    1.1. UI
+    1.2. Handle user clicks on navigation items
+2. SignUp
+    2.1. SignUp route
+    2.2. SignupForm
+    2.3. Handle signup success/failure
+3. SignIn
+    2.1. SignIn route
+    2.2. SignInForm
+    2.3. Handle signIn success/failure
+
+4. Handle AuthState in
+    4.1 Menu
+    4.2 Navigation
+5. Route login check
+   1. Profile
+
+Optional:
+
+5. Map route to history state
+
